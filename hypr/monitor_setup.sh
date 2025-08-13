0ac7d858efd10167e0c5b110c85f04ae4855c1d6
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-13 19:25:24 +0530

    fixed bug with hypr config. option to reload monitors
    monitor setup script was causing a weird bug that did not allow other
    programs to start.

--- hypr/monitor_setup.sh
+++ hypr/monitor_setup.sh
@@ -9,13 +9,13 @@ if [ "$MONITOR_COUNT" -eq 2 ]; then
     
     # 1. Turn off the external monitor ("HDMI-A-1")
     hyprctl keyword monitor "HDMI-A-1, disable"
-
     # 2. Turn off the laptop monitor ("eDP-1")
     hyprctl keyword monitor "eDP-1, disable"
 
     # 3. Turn on the external monitor with resolution 1366x768
+    sleep 1
     hyprctl keyword monitor "HDMI-A-1, 1366x768@60, 0x0, 1"
-
+    sleep 1
     # 4. Turn on the internal monitor with resolution 1920x1080
     hyprctl keyword monitor "eDP-1, 1920x1080@60, 1366x0, 1"
 
