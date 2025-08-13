8a5769919c0a8b9fb3ce3b9f1354225124555214
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-08-13 07:00:44 +0530

    added additional hyprland keybinds

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 sudo cp configuration.nix /etc/nixos/configuration.nix
+
 cp nvim/init.lua ~/.config/nvim/init.lua
+
+cp hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+cp hyprland/monitor_setup.sh ~/.config/hypr/monitor_setup.sh
