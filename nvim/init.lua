dd3c9eca12c6c5cac629f9ddd6d2a2c3fdb82763
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-18 14:13:06 +0530

    added astronvim configuration files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,27 @@
--- Set leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- General settings
-vim.opt.number = true             -- Show absolute line numbers
-vim.opt.relativenumber = true     -- Show relative line numbers
-vim.opt.syntax = 'on'             -- Enable syntax highlighting
-vim.opt.termguicolors = true      -- Enable true colors in the terminal
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
+end
 
--- Indentation
-vim.opt.tabstop = 4               -- Number of spaces a tab counts for
-vim.opt.shiftwidth = 4            -- Number of spaces for indent
-vim.opt.expandtab = true          -- Use spaces instead of tabs
+vim.opt.rtp:prepend(lazypath)
 
--- Search settings
-vim.opt.ignorecase = true         -- Ignore case in search patterns
-vim.opt.smartcase = true          -- Smart case sensitive search
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
 
--- Use system clipboard
-vim.opt.clipboard = 'unnamedplus' -- Use the system clipboard for all operations
-
--- Mappings
--- jk for escape
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+require "lazy_setup"
+require "polish"
