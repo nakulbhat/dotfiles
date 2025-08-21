0dd7f24484ae0b81b026966e2fbb13461427a758
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-08-21 12:40:56 +0530

    added languages for treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,8 +7,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "lua",
-      "vim",
+      "lua", "vim", "nix", "make", "regex", 
+      "python", "java",
+      "csv", "json", "tsv", "yaml", 
+      "c", "cpp",
+      "sql",
+      "latex", "bibtex",
+      "git_rebase", "git_config", "gitattributes", "gitcommit", "gitignore",
+      "javascript", "html", "css"
       -- add more arguments for adding more treesitter parsers
     },
   },
