14770afe692fc244127a3af4f5182067789fa602
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-22 21:42:25 +0530

    added treesitter grammars

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -29,4 +29,22 @@ require("lazy").setup({
                         vim.cmd.colorscheme("catppuccin-mocha")
                 end
         },
+        {
+                "nvim-treesitter/nvim-treesitter"
+        },
 })
+
+require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+                "c",
+                "cpp",
+                "lua",
+                "markdown",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+        },       
+}
