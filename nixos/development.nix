779e58a641c0441d77f1296a40932840d564309a
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-23 06:39:25 +0530

    added lqxt polkit

--- nixos/development.nix
+++ nixos/development.nix
@@ -1,42 +1,42 @@
 { config, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    wl-clipboard
-      neovim
-      git
-      vscodium
-      pinentry-curses
-      gnupg
-      jq
-      gcc
-      gnumake
-      ltex-ls-plus
-      pyright
-      lua-language-server
-        oh-my-posh
-      (python3.withPackages (python-pkgs: with python-pkgs; [
-                             pandas
-                             jupyter
-                             notebook
-                             nbclassic
-                             jupyter-console
-                             scikit-learn
-                             numpy
-                             rdkit
-                             requests
-      ]))  
-  ];
-  programs = {
-    zsh = {
-      enable = true;
-      ohMyZsh = {
-        enable = true;
-        theme = "candy";
-      };
-    };
-  };
-  programs.zsh.syntaxHighlighting.enable = true;
-  programs.zsh.autosuggestions.enable = true;
-  users.defaultUserShell = pkgs.zsh;
+        environment.systemPackages = with pkgs; [
+                wl-clipboard
+                neovim
+                git
+                vscodium
+                pinentry-curses
+                gnupg
+                jq
+                gcc
+                gnumake
+                ltex-ls-plus
+                pyright
+                lua-language-server
+                oh-my-posh
+                (python3.withPackages (python-pkgs: with python-pkgs; [
+                        pandas
+                        jupyter
+                        notebook
+                        nbclassic
+                        jupyter-console
+                        scikit-learn
+                        numpy
+                        rdkit
+                        requests
+                ]))  
+        ];
+        programs = {
+                zsh = {
+                        enable = true;
+                        ohMyZsh = {
+                                enable = true;
+                                theme = "candy";
+                        };
+                };
+        };
+        programs.zsh.syntaxHighlighting.enable = true;
+        programs.zsh.autosuggestions.enable = true;
+        users.defaultUserShell = pkgs.zsh;
 
 }
