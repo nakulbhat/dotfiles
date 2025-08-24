66a4daf27e1c4bc8bfd3b8f0ea2f5a5a17438f50
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-24 22:13:48 +0530

    added nvim plugins and keybinds

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,13 @@
 return {
         "neovim/nvim-lspconfig",
         config = function()
-                local lspconfig = require("lspconfig")
-                lspconfig.pyright.setup({})
-                lspconfig.lua_ls.setup({})
-                lspconfig.ltex.setup({})
-                lspconfig.clangd.setup({})
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-                vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
+                local lspconfig = require "lspconfig"
+                lspconfig.pyright.setup {}
+                lspconfig.lua_ls.setup {}
+                lspconfig.ltex.setup {}
+                lspconfig.clangd.setup {}
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
 }
