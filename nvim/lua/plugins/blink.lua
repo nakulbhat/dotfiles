5cff694f6b33a63b1d79847e0ccde75c9ab01859
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-08-24 09:37:00 +0530

    added telescope and blink

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,26 @@
+return {
+        'saghen/blink.cmp',
+        dependencies = { 
+                'rafamadriz/friendly-snippets',
+        },
+
+        -- use a release tag to download pre-built binaries
+        version = '1.*',
+        opts = {
+                keymap = { preset = 'default' },
+
+                appearance = {
+                        nerd_font_variant = 'mono'
+                },
+
+                completion = { documentation = { auto_show = true } },
+                signature = { enabled = true},
+                sources = {
+                        default = { 'lsp', 'path', 'snippets', 'buffer' },
+                },
+
+                fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+        opts_extend = { "sources.default" }
+}
+
