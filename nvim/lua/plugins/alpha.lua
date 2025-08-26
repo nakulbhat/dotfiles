2f8a8b9425def8d49b80bfda3d3d898b51bf8b64
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-08-26 15:09:16 +0530

    fix(nvim-alpha): removed deprecated ref to telescope

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -22,8 +22,7 @@ return {
                 dashboard.section.buttons.val = {
                         dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
                         dashboard.button("f", "󰈞  > Find file", ":FzfLua files<CR>"),
-                        dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-                        -- dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+                        dashboard.button("r", "  > Recent", ":FzfLua oldfiles<CR>"),
                         dashboard.button("q", "󰈆  > Quit NVIM", ":qa<CR>"),
                 }
 
