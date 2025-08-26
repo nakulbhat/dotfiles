fa80851e9944e2f3d7039b9dac9c839cbe108686
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-26 13:18:45 +0530

    feat(nvim, nixos): added fzf, removed telescope

--- nixos/development.nix
+++ nixos/development.nix
@@ -3,6 +3,7 @@
         environment.systemPackages = with pkgs; [
                 wl-clipboard
                 ripgrep
+                fzf
                 neovim
                 black
                 isort
---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,13 @@
+return {
+        "ibhagwan/fzf-lua",
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- or if using mini.icons/mini.nvim
+        -- dependencies = { "echasnovski/mini.icons" },
+        config = function()
+                require("fzf-lua").setup { "fzf-native" }
+                vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
+                vim.keymap.set("n", "<leader>fg", ":FzfLua grep_curbuf<CR>")
+        end,
+        opts = {},
+}
