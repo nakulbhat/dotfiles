1d87fb0f6d968760338d1a0218ef575e2f27479b
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-26 07:32:54 +0530

    feat(hypr): setup hyprpaper and randomiser

---
+++ hypr/scripts/walls_randomiser.sh
@@ -0,0 +1,6 @@
+conf_dir=~/.config/hypr
+wp_dir=~/.config/hypr/walls
+# Pick random wallpaper
+random_pick=$(find "$wp_dir" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
+# Replace the placeholder in template and save to .conf file
+sed -e "s~<wp>~${random_pick}~g" $conf_dir/hyprpaper.template > $conf_dir/hyprpaper.conf
