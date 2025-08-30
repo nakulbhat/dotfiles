1f502ffcb5e3dc93e276b3f8dd8d37a41c0d15a2
Nakul Bhat <nakulbhat034@gmail.com>
2025-08-30 14:29:55 +0530

    feat(mini-nvim): added some basic plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require("config.visualopts")
 require("config.behavopts")
 require("config.lazy")
-
+require("config.miniModules")
 
 -- Testing: Might be changed --
 vim.opt.wrap = false -- might have problems with latex
