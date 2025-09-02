6573f9a9636ce24d0ebe073a49b58426f67f4bc4
nakulbhat <nakulbhat034@gmail.com>
2025-09-02 11:24:18 +0530

    feat(term-nvim): terminal setup complete

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,6 +1,12 @@
 return {
-    'rebelot/terminal.nvim',
-    config = function()
-        require("terminal").setup()
-    end
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = function()
+                require('toggleterm').setup {}
+                vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>")
+                vim.keymap.set("n", "<leader>tf", ":ToggleTerm direction=float<CR>")
+                vim.keymap.set("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>")
+                vim.keymap.set("n", "<leader>tv", ":ToggleTerm direction=vertical<CR>")
+                vim.keymap.set("n", "<leader>ta", ":ToggleTermToggleAll<CR>")
+        end
 }
