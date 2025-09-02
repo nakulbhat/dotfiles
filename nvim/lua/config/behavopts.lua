0584588cd49f4d59c402e95bf52aa28515be3d7a
Nakul Bhat <nakulbhat034@gmail.com>
2025-09-02 16:39:36 +0530

    feat(babar-nvim): removed barbar

--- nvim/lua/config/behavopts.lua
+++ nvim/lua/config/behavopts.lua
@@ -18,11 +18,10 @@ end
 
 vim.keymap.set("n", "<leader>cf", FormatAndIndent, { desc = "Format with LSP/null-ls and auto-indent" })
 
-vim.api.nvim_set_keymap("n", "<leader>w", ":write<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>x", ":wqall<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>q", ":quit!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ww", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wq", ":wq<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>qa", ":q!<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>bc", ":BufferClose<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<CR>", "mao<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line below" })
 vim.keymap.set("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line above" })
@@ -30,32 +29,6 @@ vim.keymap.set("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "A
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Move to previous/next
-map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
-
--- Re-order to previous/next
-map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
-
--- Goto buffer in position...
-map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
-map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
-map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
-map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
-map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
-map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
-map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
-map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
-map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
-map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
-
--- Pin/unpin buffer
-map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
-
--- Magic buffer-picking mode
-map("n", "<Space>bb", "<Cmd>BufferPick<CR>", opts)
-map("n", "<Space>bd", "<Cmd>BufferPickDelete<CR>", opts)
 
 vim.opt.mouse = ""
 vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
