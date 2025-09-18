d63387613f464d2e63768fda9d52470dfa0bff06
Nakul Bhat <nakulbhat034@gmail.com>
2025-09-18 13:02:35 +0530

    feat(zotero): added zotero

--- nixos/modules/apps.nix
+++ nixos/modules/apps.nix
@@ -3,6 +3,7 @@
         environment.systemPackages = with pkgs; [
                 yazi
                 zathura
+                zotero
                 texlive.combined.scheme-full
                 vlc
                 youtube-music
