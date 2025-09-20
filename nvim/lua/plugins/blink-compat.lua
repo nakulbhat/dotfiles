9297f3868078d6bb12787e220a00c2d85f3473b7
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-09-20 10:38:40 +0530

    fix(vimtex): cmp compat fixed

--- nvim/lua/plugins/blink-compat.lua
+++ nvim/lua/plugins/blink-compat.lua
@@ -1,41 +1,30 @@
-return {
-  -- add blink.compat
-  {
-    'saghen/blink.compat',
-    -- use v2.* for blink.cmp v1.*
-    version = '2.*',
-    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-    lazy = true,
-    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-    opts = {},
-  },
+local E = { "saghen/blink.cmp" }
 
-  {
-    'saghen/blink.cmp',
-    version = '1.*',
-    dependencies = {
-      -- add source
-      { 'micangl/cmp-vimtex' },
-    },
-    sources = {
-      -- remember to enable your providers here
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'cmp_vimtex' },
-      providers = {
-        -- create provider
-        cmp_vimtex = {
-          -- IMPORTANT: use the same name as you would for nvim-cmp
-          name = 'cmp_vimtex',
-          module = 'blink.compat.source',
-
-          -- all blink.cmp source config options work as normal:
-          score_offset = -3,
-
-          -- this table is passed directly to the proxied completion source
-          -- as the `option` field in nvim-cmp's source config
-          --
-          -- this is NOT the same as the opts in a plugin's lazy.nvim spec
+E.dependencies = {
+        {
+                "micangl/cmp-vimtex",
+                dependencies = {
+                        {
+                                "saghen/blink.compat",
+                                version = "*",
+                                lazy = true,
+                                opts = {},
+                        },
+                },
         },
-      },
-    },
-  },
 }
+
+E.opts = {
+        sources = {
+                default = { "vimtex" },
+                providers = {
+                        vimtex = {
+                                name = "vimtex",
+                                module = "blink.compat.source",
+                                score_offset = 100,
+                        },
+                },
+        },
+}
+
+return { E }
