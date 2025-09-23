5db65feadc8c16bdd9838a0369e0a69117c04457
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-09-23 07:52:36 +0530

    feat(apps): added inkscape

--- nixos/modules/apps.nix
+++ nixos/modules/apps.nix
@@ -3,6 +3,7 @@
         environment.systemPackages = with pkgs; [
                 yazi
                 zathura
+                inkscape
                 zotero
                 texlive.combined.scheme-full
                 vlc
