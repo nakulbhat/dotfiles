252b990f829af60f76e812a9cba6a722614284dd
Nakul Bhat <nakulbhat034@gmail.com>
2025-10-06 12:05:05 +0530

    feat(nvim): added zen mode

--- nvim/lua/config/snacksOpts.lua
+++ nvim/lua/config/snacksOpts.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', '<leader>sde', Snacks.dim.enable)
 vim.keymap.set('n', '<leader>sdd', Snacks.dim.disable)
 
 vim.keymap.set('n', '<leader>lg', Snacks.lazygit.open)
+vim.keymap.set('n', '<leader>s ', Snacks.zen.zen)
