20b831282a627e2c6b44f6018a3d8bc46521fb33
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-10-06 10:30:14 +0530

    feat(snacks-dim): added dim option with snacks

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("config.behavopts")
 require("config.lazy")
 require("config.miniModules")
 require('config.ironConfig')
+require('config.snacksOpts')
 
 -- Testing: Might be changed --
 vim.opt.wrap = false -- might have problems with latex
---
+++ nvim/lua/config/snacksOpts.lua
@@ -0,0 +1,4 @@
+Snacks = require("snacks")
+
+vim.keymap.set('n', '<leader>sde', Snacks.dim.enable)
+vim.keymap.set('n', '<leader>sdd', Snacks.dim.disable)
