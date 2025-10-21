397c0cf2f58cef77f6ab5abcc990d3d7d3008acf
Nakul Bhat <nakulbhat034@gmail.com>
2025-10-21 12:52:48 +0530

    refactor(nvim): changed to tabstop 4

--- nvim/lua/config/visualopts.lua
+++ nvim/lua/config/visualopts.lua
@@ -9,8 +9,8 @@ vim.opt.splitright = true -- Vsplits open to the right
 -- Indentation settings. 
 -- 8 spaces is default,
 -- but I put this here so I can edit it later.
-vim.opt.tabstop = 8 -- used when saving a file
-vim.opt.shiftwidth = 8 -- used by >> and << for the indentation
+vim.opt.tabstop = 4 -- used when saving a file
+vim.opt.shiftwidth = 4 -- used by >> and << for the indentation
 vim.opt.expandtab = true -- saving tabs as spaces
 
 vim.opt.scrolloff = 999 -- keeps the cursor in the middle of the screen.
