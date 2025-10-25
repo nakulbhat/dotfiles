dcd796407a9c6686c715a08018c69d317cd9a4aa
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-10-25 16:01:22 +0530

    fix(sound): separated pulseaudio and pipewire

--- nixos/modules/apps.nix
+++ nixos/modules/apps.nix
@@ -1,30 +1,32 @@
 { config, pkgs, ... }:
 {
-        environment.systemPackages = with pkgs; [
-                iwgtk
-                rclone
-                yazi
-                zathura
-                krusader
-                inkscape
-                zotero
-                texlive.combined.scheme-full
-                vlc
-                youtube-music
-        ];
-        nixpkgs.config.allowUnfree = true;
-        programs.firefox.enable = true;
+    environment.systemPackages = with pkgs; [
+        iwgtk
+        rclone
+        yazi
+        zathura
+        krusader
+        inkscape
+        zotero
+        texlive.combined.scheme-full
+        vlc
+        youtube-music
+    ];
+    nixpkgs.config.allowUnfree = true;
+    programs.firefox.enable = true;
 
-        services.printing.enable = true;
+    services.printing.enable = true;
 
-        services.desktopManager.plasma6.enable = true;
-        services.pulseaudio.enable = false;
-        security.rtkit.enable = true;
-        services.pipewire = {
-                enable = true;
-                alsa.enable = true;
-                alsa.support32Bit = true;
-                pulse.enable = true;
-        };
+    services.desktopManager.plasma6.enable = true;
+
+
+    # Either disable PipeWire completely or just audio
+    services.pipewire = {
+        enable = true;        # if you want PipeWire for video or MIDI
+        alsa.enable = false;  # disable PipeWire ALSA
+        pulse.enable = false; # disable PipeWire PulseAudio replacement
+    };
+    hardware.pulseaudio.enable = true;
+    hardware.pulseaudio.support32Bit = true; 
 }
 
