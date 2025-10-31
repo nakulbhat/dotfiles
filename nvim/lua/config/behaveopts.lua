04b5cb7e26895530637fb9809819747a4de08a9d
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-10-31 21:37:12 +0530

    refactor(nvim): setup move, leap, behaveopts and visualopts

--- nvim/lua/config/behaveopts.lua
+++ nvim/lua/config/behaveopts.lua
@@ -7,13 +7,14 @@ vim.opt.inccommand = "split" -- shows a preview of all changes in a separate bot
 vim.opt.ignorecase = true -- allow ignorecase in command line
 
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 function FormatAndIndent()
-        vim.lsp.buf.format {
-                async = false,
-                timeout_ms = 1000,
-        }
-        vim.cmd.normal "gg=G"
+    vim.lsp.buf.format {
+        async = false,
+        timeout_ms = 1000,
+    }
+    vim.cmd.normal "gg=G"
 end
 
 vim.keymap.set("n", "<leader>cf", FormatAndIndent, { desc = "Format with LSP/null-ls and auto-indent" })
@@ -45,3 +46,26 @@ vim.opt.spelllang = 'en_gb'
 
 vim.keymap.set('i', '<C-f>', [[<C-g>u<Esc>[s1z=`]a<C-g>u]], { noremap = true, desc = "Fix previous spelling-like change in insert mode" })
 vim.keymap.set('n', '<C-f>', "[s1z=<C-o>", { noremap = true, desc = "Run the [s1z= command then jump back" })
+
+-- Requires plugin fedepujol/move.nvim
+local opts = { noremap = true, silent = true }
+-- Normal-mode commands
+vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+
+-- Visual-mode commands
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
+
+require("fzf-lua").setup { "fzf-native" }
+vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
+vim.keymap.set("n", "<leader>fg", ":FzfLua lgrep_curbuf<CR>")
+vim.keymap.set("n", "<leader>fv", ":vnew<CR>:FzfLua files<CR>")
+vim.keymap.set("n", "<leader>fh", ":new<CR>:FzfLua files<CR>")
+vim.keymap.set("n", "<leader>ft", ":FzfLua tabs<CR>")
