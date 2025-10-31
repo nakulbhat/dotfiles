c8df221d19b186c0e6dccf6ceaf68da4cd48feef
Nakul Bhat <nakulbhat034@gmail.com>
2025-10-31 21:37:12 +0530

    refactor(nvim): setup move, leap, behaveopts and visualopts

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,18 @@
 local vim = vim
 local Plug = vim.fn["plug#"]
 
+require("config.behaveopts")
+require("config.visualopts")
+
 vim.call("plug#begin")
 Plug("nvim-tree/nvim-web-devicons")
 Plug("ibhagwan/fzf-lua")
 Plug("numToStr/Comment.nvim")
-
+Plug("fedepujol/move.nvim")
+Plug 'Aasim-A/scrollEOF.nvim'
+Plug 'sphamba/smear-cursor.nvim'
+Plug 'kylechui/nvim-surround'
+Plug("ggandor/leap.nvim")
 vim.call("plug#end")
+require("move").setup({})
+require("nvim-surround").setup({})
