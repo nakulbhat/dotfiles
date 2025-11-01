b9402b726000d79dfc21c3e0f176f4f71f769588
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-11-01 07:39:10 +0530

    refactor(nvim): more consolidation and comments

--- nvim/lua/config/behaveopts.lua
+++ nvim/lua/config/behaveopts.lua
@@ -1,13 +1,14 @@
 vim.opt.clipboard = "unnamedplus"
-
 vim.opt.virtualedit = "block" -- allows cursor to go off CR in visual BLOCK mode.
-
 vim.opt.inccommand = "split" -- shows a preview of all changes in a separate bottom split.
-
 vim.opt.ignorecase = true -- allow ignorecase in command line
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.opt.spell = true
+vim.opt.spelllang = 'en_gb'
+vim.keymap.set('i', '<C-f>', [[<C-g>u<Esc>[s1z=`]a<C-g>u]], { noremap = true, desc = "Fix previous spelling-like change in insert mode" })
+vim.keymap.set('n', '<C-f>', "[s1z=<C-o>", { noremap = true, desc = "Run the [s1z= command then jump back" })
 
 function FormatAndIndent()
     vim.lsp.buf.format {
@@ -16,56 +17,42 @@ function FormatAndIndent()
     }
     vim.cmd.normal "gg=G"
 end
-
 vim.keymap.set("n", "<leader>cf", FormatAndIndent, { desc = "Format with LSP/null-ls and auto-indent" })
 
-local opts = { noremap = true, silent = true, nowait = true }
+local no_wait_opts = { noremap = true, silent = true, nowait = true }
+local general_opts = { noremap = true, silent = true}
 
--- Save the current file using Alt + w
-vim.keymap.set('n', '<A-w>', ':w<CR>', opts)
+--saving, quitting and closing
+vim.keymap.set('n', '<A-w>', ':w<CR>', no_wait_opts)
+vim.keymap.set('n', '<A-q>', ':qa!<CR>', no_wait_opts)
+vim.keymap.set('n', '<A-c>', ':bd!<CR>', no_wait_opts)
+vim.keymap.set('n', '<A-x>', ':wqall<CR>', no_wait_opts)
 
--- Quit Neovim without saving all buffers using Alt + q
-vim.keymap.set('n', '<A-q>', ':qa!<CR>', opts)
+-- adding empty lines above and below current line
+vim.keymap.set("n", "<CR>", "mao<esc>0<S-d>`a<cmd>delmarks a<cr>")
+vim.keymap.set("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>")
 
--- Close current buffer without saving using Alt + c
-vim.keymap.set('n', '<A-c>', ':bd!<CR>', opts)
+-- moving to different visible windows
+vim.keymap.set('n', '<C-h>', '<C-w>h', general_opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', general_opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', general_opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', general_opts)
 
--- Save and quit all open buffers using Alt + x
-vim.keymap.set('n', '<A-x>', ':wqall<CR>', opts)
 
-vim.keymap.set("n", "<CR>", "mao<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line below" })
-vim.keymap.set("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line above" })
-
-vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
-
-vim.opt.spell = true
-vim.opt.spelllang = 'en_gb'
-
-vim.keymap.set('i', '<C-f>', [[<C-g>u<Esc>[s1z=`]a<C-g>u]], { noremap = true, desc = "Fix previous spelling-like change in insert mode" })
-vim.keymap.set('n', '<C-f>', "[s1z=<C-o>", { noremap = true, desc = "Run the [s1z= command then jump back" })
-
--- Requires plugin fedepujol/move.nvim
-local opts = { noremap = true, silent = true }
--- Normal-mode commands
-vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
-vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
-vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
-vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
-vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
-vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
-
--- Visual-mode commands
-vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
-vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
-vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
+require("move").setup({})
+vim.keymap.set({'n','v'}, '<A-j>', ':MoveLine(1)<CR>', general_opts)
+vim.keymap.set({'n','v'}, '<A-k>', ':MoveLine(-1)<CR>', general_opts)
+vim.keymap.set({'n','v'}, '<A-h>', ':MoveHChar(-1)<CR>', general_opts)
+vim.keymap.set({'n','v'}, '<A-l>', ':MoveHChar(1)<CR>', general_opts)
+vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', general_opts)
+vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', general_opts)
 
 require("fzf-lua").setup { "fzf-native" }
 vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>")
-vim.keymap.set("n", "<leader>fg", ":FzfLua lgrep_curbuf<CR>")
+vim.keymap.set("n", "<leader>fg", ":FzfLua live_grep<CR>")
+vim.keymap.set("n", "<leader>fb", ":FzfLua lgrep_curbuf<CR>")
 vim.keymap.set("n", "<leader>fv", ":vnew<CR>:FzfLua files<CR>")
 vim.keymap.set("n", "<leader>fh", ":new<CR>:FzfLua files<CR>")
-vim.keymap.set("n", "<leader>ft", ":FzfLua tabs<CR>")
+
+vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)', { silent = true })
+vim.keymap.set('n', 'S', '<Plug>(leap-from-window)', { silent = true })
--- nvim/lua/config/visualopts.lua
+++ nvim/lua/config/visualopts.lua
@@ -1,23 +1,18 @@
--- Solid: Unlikely to be changed --
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.splitbelow = true -- Stuff like the help menu opens at the bottom
 vim.opt.splitright = true -- Vsplits open to the right
-
--- Indentation settings. 
--- 8 spaces is default,
--- but I put this here so I can edit it later.
 vim.opt.tabstop = 4 -- used when saving a file
 vim.opt.shiftwidth = 4 -- used by >> and << for the indentation
 vim.opt.expandtab = true -- saving tabs as spaces
-
 vim.opt.termguicolors = true
+vim.opt.cmdheight = 1
 
-vim.opt.cmdheight = 0
 vim.diagnostic.config({ virtual_text = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
     end,
 })
+
+require("smear_cursor").enabled = true
