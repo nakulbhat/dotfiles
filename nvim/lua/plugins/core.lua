d11b1112458f058b8c657a0f6ef8ccaafbfeafea
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-11-01 13:46:13 +0530

    refactor(nvim): added iron

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,55 +1,55 @@
 return {
-  "nvim-tree/nvim-web-devicons",
-  "neovim/nvim-lspconfig",
-  "ibhagwan/fzf-lua",
-  "numToStr/Comment.nvim",
-  "fedepujol/move.nvim",
-  "Aasim-A/scrollEOF.nvim",
-  "sphamba/smear-cursor.nvim",
-  "ggandor/leap.nvim",
-  "nvim-lua/plenary.nvim",
-  "mikavilpas/yazi.nvim",
-  "romainl/vim-cool",
-  "lervag/vimtex",
+    "nvim-tree/nvim-web-devicons",
+    "neovim/nvim-lspconfig",
+    "ibhagwan/fzf-lua",
+    "numToStr/Comment.nvim",
+    "fedepujol/move.nvim",
+    "Aasim-A/scrollEOF.nvim",
+    "sphamba/smear-cursor.nvim",
+    "ggandor/leap.nvim",
+    "nvim-lua/plenary.nvim",
+    "mikavilpas/yazi.nvim",
+    "romainl/vim-cool",
+    "lervag/vimtex",
+    "Vigemus/iron.nvim",
+    {
+        "kylechui/nvim-surround",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
 
-  {
-    "kylechui/nvim-surround",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
+    {
+        "folke/snacks.nvim",
+        config = function()
+            require("snacks").setup({
+                lazygit = { enabled = true },
+            })
+        end,
+    },
 
-  {
-    "folke/snacks.nvim",
-    config = function()
-      require("snacks").setup({
-        lazygit = { enabled = true },
-      })
-    end,
-  },
+    {
+        "nvim-mini/mini.nvim",
+        config = function()
+            require("mini.ai").setup({})
+            require("mini.pairs").setup({})
+        end,
+    },
 
-  {
-    "nvim-mini/mini.nvim",
-    config = function()
-      require("mini.ai").setup({})
-      require("mini.pairs").setup({})
-    end,
-  },
+    {
+        "eoh-bse/minintro.nvim",
+        config = function()
+            require("minintro").setup({})
+        end,
+    },
 
-  {
-    "eoh-bse/minintro.nvim",
-    config = function()
-      require("minintro").setup({})
-    end,
-  },
-
-  {
-    "Ramilito/winbar.nvim",
-    config = function()
-      require("winbar").setup({
-        dir_levels = 2,
-        filetype_exclude = { "minintro" },
-      })
-    end,
-  },
+    {
+        "Ramilito/winbar.nvim",
+        config = function()
+            require("winbar").setup({
+                dir_levels = 2,
+                filetype_exclude = { "minintro" },
+            })
+        end,
+    },
 }
