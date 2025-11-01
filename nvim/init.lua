b5eeb1f1ad5bd42c1accc9fa0c9552e2c524df41
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-11-01 06:22:56 +0530

    refactor(nvim): moved to pckr

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,4 @@
-local vim = vim
-local Plug = vim.fn["plug#"]
-
+require("config.plugins")
 require("config.behaveopts")
 require("config.visualopts")
 
-vim.call("plug#begin")
-Plug("nvim-tree/nvim-web-devicons")
-Plug("ibhagwan/fzf-lua")
-Plug("numToStr/Comment.nvim")
-Plug("fedepujol/move.nvim")
-Plug 'Aasim-A/scrollEOF.nvim'
-Plug 'sphamba/smear-cursor.nvim'
-Plug 'kylechui/nvim-surround'
-Plug("ggandor/leap.nvim")
-vim.call("plug#end")
-require("move").setup({})
-require("nvim-surround").setup({})
