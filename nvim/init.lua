5e68fb4523e265f72a82b3682feee8d31206a9da
Nakul Bhat <nakulbhat034@gmail.com>
2025-11-01 13:46:13 +0530

    refactor(nvim): added iron

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " " -- lazy needs leaders to be loaded before itself
 vim.g.maplocalleader = " "
 require("config.lazy")
 require("lazy").setup("plugins")
+require('config.lsp')
+require('config.iron')
 require("config.behaveopts")
 require("config.visualopts")
 
