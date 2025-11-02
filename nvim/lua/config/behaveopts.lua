743bde07460574c6097244e02b9bc42f35793b73
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-11-02 22:16:02 +0530

    refactor(nvim): replaced snacks with lazygit

--- nvim/lua/config/behaveopts.lua
+++ nvim/lua/config/behaveopts.lua
@@ -1,21 +1,23 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.virtualedit = "block" -- allows cursor to go off CR in visual BLOCK mode.
-vim.opt.inccommand = "split" -- shows a preview of all changes in a separate bottom split.
-vim.opt.ignorecase = true -- allow ignorecase in command line
+vim.opt.inccommand = "split"  -- shows a preview of all changes in a separate bottom split.
+vim.opt.ignorecase = true     -- allow ignorecase in command line
 
 vim.opt.spell = true
 vim.opt.spelllang = 'en_gb'
-vim.keymap.set('i', '<C-f>', [[<C-g>u<Esc>[s1z=`]a<C-g>u]], { noremap = true, desc = "Fix previous spelling-like change in insert mode" })
+vim.keymap.set('i', '<C-f>', [[<C-g>u<Esc>[s1z=`]a<C-g>u]],
+    { noremap = true, desc = "Fix previous spelling-like change in insert mode" })
 vim.keymap.set('n', '<C-f>', "[s1z=<C-o>", { noremap = true, desc = "Run the [s1z= command then jump back" })
 
 function FormatAndIndent()
     vim.lsp.buf.format { async = false, timeout_ms = 1000, }
     vim.cmd.normal "gg=G"
 end
+
 vim.keymap.set("n", "<leader>cf", FormatAndIndent, { desc = "Format with LSP/null-ls and auto-indent" })
 
 local no_wait_opts = { noremap = true, silent = true, nowait = true }
-local general_opts = { noremap = true, silent = true}
+local general_opts = { noremap = true, silent = true }
 
 --saving, quitting and closing
 vim.keymap.set('n', '<A-w>', ':w<CR>', no_wait_opts)
@@ -36,10 +38,10 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', general_opts)
 
 -- Moving lines
 require("move").setup({})
-vim.keymap.set({'n','v'}, '<A-j>', ':MoveLine(1)<CR>', general_opts)
-vim.keymap.set({'n','v'}, '<A-k>', ':MoveLine(-1)<CR>', general_opts)
-vim.keymap.set({'n','v'}, '<A-h>', ':MoveHChar(-1)<CR>', general_opts)
-vim.keymap.set({'n','v'}, '<A-l>', ':MoveHChar(1)<CR>', general_opts)
+vim.keymap.set({ 'n', 'v' }, '<A-j>', ':MoveLine(1)<CR>', general_opts)
+vim.keymap.set({ 'n', 'v' }, '<A-k>', ':MoveLine(-1)<CR>', general_opts)
+vim.keymap.set({ 'n', 'v' }, '<A-h>', ':MoveHChar(-1)<CR>', general_opts)
+vim.keymap.set({ 'n', 'v' }, '<A-l>', ':MoveHChar(1)<CR>', general_opts)
 vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', general_opts)
 vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', general_opts)
 
@@ -59,12 +61,10 @@ vim.keymap.set('n', 'S', '<Plug>(leap-from-window)', { silent = true })
 require("yazi").setup({})
 vim.keymap.set("n", "<leader>-", function() require("yazi").yazi() end)
 
--- snacks: lazygit
-Snacks = require("snacks")
-vim.keymap.set('n', '<leader>lg', Snacks.lazygit.open)
+vim.keymap.set('n', '<leader>lg', "<cmd>LazyGit<cr>")
 
 -- mini splitjoin
-require("mini.splitjoin").setup {mappings = {toggle = "<leader> "}}
+require("mini.splitjoin").setup { mappings = { toggle = "<leader> " } }
 
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_compiler_method = "arara"
