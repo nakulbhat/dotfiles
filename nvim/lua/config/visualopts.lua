56abda9831894ef10c5bbbfe4d94257f5a220c5a
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-11-02 22:08:33 +0530

    feat(nvim): added lualine

--- nvim/lua/config/visualopts.lua
+++ nvim/lua/config/visualopts.lua
@@ -2,12 +2,12 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.splitbelow = true -- Stuff like the help menu opens at the bottom
 vim.opt.splitright = true -- Vsplits open to the right
-vim.opt.tabstop = 4 -- used when saving a file
-vim.opt.shiftwidth = 4 -- used by >> and << for the indentation
-vim.opt.expandtab = true -- saving tabs as spaces
+vim.opt.tabstop = 4       -- used when saving a file
+vim.opt.shiftwidth = 4    -- used by >> and << for the indentation
+vim.opt.expandtab = true  -- saving tabs as spaces
 vim.opt.termguicolors = true
-vim.opt.cmdheight=1
-vim.opt.scrolloff=10
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 10
 
 vim.diagnostic.config({ virtual_text = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -18,3 +18,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 require("smear_cursor").enabled = true
 vim.cmd('colorscheme monokai-pro')
+
+require("lualine").setup {
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'filetype', 'lsp_status' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+}
