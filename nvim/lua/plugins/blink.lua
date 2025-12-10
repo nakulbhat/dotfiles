ab30f6adffc28b0ef44e3845fda9f272166ce5e8
Nakul Bhat <nakulbhat034@gmail.com>
2025-12-10 08:48:09 +0530

    fix(nvim): fixed vimtex suggestions

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -5,21 +5,24 @@ return {
         "L3MON4D3/LuaSnip",
         "rafamadriz/friendly-snippets",
     },
-    config = function()
-        require("blink.cmp").setup({
-            keymap = { preset = "super-tab" },
-            appearance = { nerd_font_variant = "mono" },
-            completion = {
-                documentation = { auto_show = true },
-                selection = { auto_insert = false },
-                trigger = { show_in_snippet = false },
+    opts = {
+        keymap = { preset = "super-tab" },
+        appearance = { nerd_font_variant = "mono" },
+        completion = {
+            documentation = { auto_show = true },
+            selection = { auto_insert = false },
+            trigger = { show_in_snippet = false },
+        },
+        signature = { enabled = true },
+        snippets = { preset = "luasnip" },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            providers = {
+                snippets = { score_offset = 150 },
+                lsp = { score_offset = 100 },
             },
-            signature = { enabled = true },
-            snippets = { preset = "luasnip" },
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
-            },
-            fuzzy = { implementation = "prefer_rust_with_warning" },
-        })
-    end,
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default"}
 }
