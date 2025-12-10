b5dd564c6b8ef09191fe9c44631b3bbd501c9ddf
Nakul Bhat <nakulbhatexternal@gmail.com>
2025-12-10 08:48:09 +0530

    fix(nvim): fixed vimtex suggestions

--- nvim/lua/plugins/blink_compat.lua
+++ nvim/lua/plugins/blink_compat.lua
@@ -1,30 +1,42 @@
+local M = { "lervag/vimtex" }
+
+M.lazy = false
+
+M.init = function()
+    vim.g.tex_flavor = "latex"
+    vim.g.vimtex_indent_enabled = 0
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_automatic = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
+end
+
 local E = { "saghen/blink.cmp" }
 
 E.dependencies = {
-        {
-                "micangl/cmp-vimtex",
-                dependencies = {
-                        {
-                                "saghen/blink.compat",
-                                version = "*",
-                                lazy = true,
-                                opts = {},
-                        },
-                },
-        },
+{
+    "micangl/cmp-vimtex",
+    dependencies = {
+    {
+        "saghen/blink.compat",
+        version = "*",
+        lazy = true,
+        opts = {},
+    },
+    },
+},
 }
 
 E.opts = {
-        sources = {
-                default = { "vimtex" },
-                providers = {
-                        vimtex = {
-                                name = "vimtex",
-                                module = "blink.compat.source",
-                                score_offset = 100,
-                        },
-                },
-        },
+sources = {
+    default = { "vimtex" },
+    providers = {
+    vimtex = {
+        name = "vimtex",
+        module = "blink.compat.source",
+        score_offset = 100,
+    },
+    },
+},
 }
 
-return { E }
+return { M, E }
