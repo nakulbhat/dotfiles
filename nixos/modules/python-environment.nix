89ad11e36e12fea3fc7aafbd28edf0b524c543a4
Nakul Bhat <nakulbhat034@gmail.com>
2026-01-27 12:56:37 +0530

    refactor(nixos): minimal python env

--- nixos/modules/python-environment.nix
+++ nixos/modules/python-environment.nix
@@ -3,20 +3,10 @@
         environment.systemPackages = with pkgs; [
                 (python3.withPackages (python-pkgs: with python-pkgs; [
                         pandas
-                        flask
-                        cairosvg
-                        jupyter
-                        flask-limiter
                         notebook
                         matplotlib
-                        opencv-python
-                        pyspark
                         virtualenv
-                        nbclassic
-                        jupyter-console
-                        scikit-learn
                         numpy
-                        rdkit
                         requests
                 ]))  
         ];
