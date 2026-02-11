afc9ca3ecd52d53568890b30f5be196308d17538
Nakul Bhat <nakulbhat034@gmail.com>
2026-02-11 13:55:27 +0530

    feat(nixos): add substituters for cuda

--- nixos/modules/shell.nix
+++ nixos/modules/shell.nix
@@ -1,39 +1,50 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
-        environment.systemPackages = with pkgs; [
-                wl-clipboard
-                oh-my-posh
-                gcalcli
-                unzip
-                zip
-                poppler-utils
-                rip2
-                ripgrep
-                zoxide
-                pandoc
-                curl
-                entr
-                power-profiles-daemon
-                yazi
-                zathura
-                wget
-                lxqt.lxqt-policykit
-                font-awesome
-                nerd-fonts.jetbrains-mono
-                bat
-                gnumake
-                fzf
-                fd
-                tmux
-                pinentry-curses
-                gnupg
-                jq
+    environment.systemPackages = with pkgs; [
+        wl-clipboard
+        oh-my-posh
+        gcalcli
+        unzip
+        zip
+        poppler-utils
+        rip2
+        ripgrep
+        zoxide
+        pandoc
+        curl
+        entr
+        power-profiles-daemon
+        yazi
+        zathura
+        wget
+        lxqt.lxqt-policykit
+        font-awesome
+        bat
+        gnumake
+        fzf
+        fd
+        tmux
+        pinentry-curses
+        gnupg
+        jq
+        tmux-sessionizer
+    ];
+    programs.gnupg.agent = {
+        enable = true;
+    };
+    programs.zsh.enable = true;
+    programs.zsh.syntaxHighlighting.enable = true;
+    programs.zsh.autosuggestions.enable = true;
+    users.defaultUserShell = pkgs.zsh;
+    environment.sessionVariables = {
+        SHELL = lib.getExe pkgs.zsh;
+    };
+    nix.settings = {
+        substituters = [
+            "https://cache.nixos-cuda.org"
         ];
-        programs.gnupg.agent = {
-                enable = true;
-        };
-        programs.zsh.enable = true;
-        programs.zsh.syntaxHighlighting.enable = true;
-        programs.zsh.autosuggestions.enable = true;
-        users.defaultUserShell = pkgs.zsh;
+        trusted-public-keys = [
+            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        ];
+    };
 }
