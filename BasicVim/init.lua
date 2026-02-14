02336c3edff75a47afd98777391e0ecb77081809
Nakul Bhat <nakulbhat034@gmail.com>
2026-02-14 10:04:48 +0530

    refactor(nvim): removed non-essential features

--- BasicVim/init.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " " -- lazy needs leaders to be loaded before itself
-vim.g.maplocalleader = " "
-require("config.lazy")
-require("lazy").setup("plugins")
-require("config.basicBehaveOpts")
-require("config.basicVisualOpts")
-
